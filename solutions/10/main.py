576389ea2008ade7083cb30bf81247c3fe5accd5
Stefan Boonstra <anve_nomuree@hotmail.com>
2022-11-07 20:16:30 +0100

    Updates problem 10 solution using sieve of eratosthenes

--- solutions/10/main.py
+++ solutions/10/main.py
@@ -3,6 +3,22 @@ Problem 10: The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million.
 """
 
+from math import sqrt
+
+def sieve_of_eratosthenes(limit: int) -> list:
+    """Use Sieve of Eratosthenes to more efficiently calculate prime"""
+    primes = {k:True for k in range(2, limit)}
+    end = sqrt(limit)
+    for i in primes.keys():
+        if i >= end:
+            break
+        
+        if primes.get(i) == True:
+            for j in range(pow(i, 2), limit, i):
+                primes[j] = False
+            
+    return primes
+
 def generate_primes(limit: int) -> list:
     """Generate primes up to a value of limit"""
     found = [2]
@@ -17,6 +33,11 @@ def generate_primes(limit: int) -> list:
     return found
     
 if __name__ == '__main__':
-    """Took 10m34s :("""
-    primes = generate_primes(1999999)
-    print(sum(primes))
+    # Brute force takes 10m34s :(
+    # primes = generate_primes(1999999)
+    # print(sum(primes))
+    
+    # Sieve takes 0.7s :D
+    primes = sieve_of_eratosthenes(2000000)
+    total = sum([key for key, is_prime in primes.items() if is_prime])
+    print(total)
